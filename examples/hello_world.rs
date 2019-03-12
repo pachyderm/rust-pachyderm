e0417c363c63266278ec241efcff4a90735b1872
Yusuf Simonson <simonson@gmail.com>
2019-03-12 18:27:35 -0400

    Cleaned up example imports

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -6,12 +6,17 @@ extern crate pachyderm;
 extern crate tokio;
 extern crate tower_add_origin;
 extern crate tower_h2;
+extern crate tower_grpc;
 extern crate tower_service;
 extern crate tower_util;
 
+use std::io::Error as IoError;
+
 use futures::{Future, Poll};
+use http::Uri;
 use tokio::executor::DefaultExecutor;
 use tokio::net::tcp::{ConnectFuture, TcpStream};
+use tower_add_origin::Builder;
 use tower_grpc::Request;
 use tower_h2::client;
 use tower_service::Service;
@@ -22,12 +27,12 @@ use pachyderm::pfs::{client::Api as PfsApi, CreateRepoRequest, Repo};
 fn main() {
     let h2_settings = Default::default();
     let mut make_client = client::Connect::new(Dest, h2_settings, DefaultExecutor::current());
-    let uri: http::Uri = "http://localhost:30650".parse().unwrap();
+    let uri: Uri = "http://localhost:30650".parse().unwrap();
 
     let callback = make_client
         .make_service(())
         .map(move |conn| {
-            let conn = tower_add_origin::Builder::new().uri(uri).build(conn).unwrap();
+            let conn = Builder::new().uri(uri).build(conn).unwrap();
 
             PfsApi::new(conn)
         })
@@ -57,7 +62,7 @@ struct Dest;
 
 impl Service<()> for Dest {
     type Response = TcpStream;
-    type Error = ::std::io::Error;
+    type Error = IoError;
     type Future = ConnectFuture;
 
     fn poll_ready(&mut self) -> Poll<(), Self::Error> {
