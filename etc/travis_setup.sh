db5796e796136756da33eff5d692e2996c2657fb
Yusuf Simonson <simonson@gmail.com>
2019-11-19 12:09:23 -0500

    Fix setup script

--- etc/travis_setup.sh
+++ etc/travis_setup.sh
@@ -8,7 +8,7 @@ which pachctl
 pushd $HOME/cached-deps/pachyderm
     make launch-kube
     pachctl deploy local
-    until timeout 1s ./proto/pachyderm/etc/kube/check_ready.sh app=pachd; do
+    until timeout 1s ./etc/kube/check_ready.sh app=pachd; do
         sleep 1;
     done
     pachctl version
