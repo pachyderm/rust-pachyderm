f87ed585a6d90a4d64b858d1e9b97dd8c3fad3dd
Yusuf Simonson <simonson@gmail.com>
2019-11-19 11:15:31 -0500

    Fixed various issues with scripts

--- etc/travis_install.sh
+++ etc/travis_install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+set -ex
+
 mkdir -p $HOME/cached-deps
 
 if [ ! -d "$HOME/cached-deps/pachyderm" ]; then
-    wget -c https://github.com/pachyderm/pachyderm/archive/v$(PACHYDERM_VERSION).zip
-    unzip v$(PACHYDERM_VERSION)
-    mv pachyderm-$(PACHYDERM_VERSION) pachyderm
+    wget -c https://github.com/pachyderm/pachyderm/archive/v${PACHYDERM_VERSION}.zip
+    unzip v${PACHYDERM_VERSION}
+    mv pachyderm-${PACHYDERM_VERSION} $HOME/cached-deps/pachyderm
 fi
 
-pushd pachyderm
-    sudo etc/testing/travis_cache.sh && \
-    sudo etc/testing/travis_install.sh && \
-    curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v$(PACHYDERM_VERSION)/pachctl_$(PACHYDERM_VERSION)_amd64.deb  && \
-    sudo dpkg -i /tmp/pachctl.deb
+pushd $HOME/cached-deps/pachyderm
+    sudo etc/testing/travis_cache.sh
+    sudo etc/testing/travis_install.sh
 popd
+
+if [ ! -f "$HOME/cached-deps/pachctl.deb" ]; then
+    curl -o $HOME/cached-deps/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v${PACHYDERM_VERSION}/pachctl_${PACHYDERM_VERSION}_amd64.deb
+fi
+sudo dpkg -i $HOME/cached-deps/pachctl.deb
