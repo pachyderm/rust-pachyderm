f19857fa2ac094fbcae0d3ff879ddc04ad7d8ada
Yusuf Simonson <simonson@gmail.com>
2020-04-27 13:18:50 -0400

    Basic fuzzing of extract/restore

--- Makefile
+++ Makefile
@@ -17,6 +17,6 @@ proto:
 
 fuzz-extract-restore:
 	pachctl create repo fuzz_extract_restore_input
-	pachctl create pipeline -f < fuzz/etc/extract_restore_output.json
+	pachctl create pipeline -f fuzz/etc/extract_restore_output.json
 	RUST_BACKTRACE=1 PACHD_ADDRESS=$(PACHD_ADDRESS) cargo fuzz run extract_restore
 	yes | pachctl delete all
--- fuzz/fuzz_targets/extract_restore.rs
+++ fuzz/fuzz_targets/extract_restore.rs
@@ -5,17 +5,19 @@ extern crate lazy_static;
 
 use std::env;
 use std::error::Error;
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use pachyderm::pfs;
 use pachyderm::pfs::api_client::ApiClient as PfsClient;
-use pachyderm::pps;
 // use pachyderm::pps::api_client::ApiClient as PpsClient;
+use pachyderm::admin;
 use pachyderm::admin::api_client::ApiClient as AdminClient;
 
 use libfuzzer_sys::fuzz_target;
-use arbitrary::{Arbitrary, Unstructured};
+use arbitrary::Arbitrary;
 use tokio::runtime::Runtime;
+use futures::stream;
+use futures::stream::TryStreamExt;
 
 lazy_static! {
     static ref COUNTER: AtomicUsize = AtomicUsize::new(0);
@@ -29,16 +31,55 @@ pub enum Op {
 
 async fn run(ops: Vec<Op>) -> Result<(), Box<dyn Error>> {
     let pachd_address = env::var("PACHD_ADDRESS")?;
-    let pfs_client = PfsClient::connect(pachd_address.clone()).await?;
-    let admin_client = AdminClient::connect(pachd_address.clone()).await?;
+    let mut pfs_client = PfsClient::connect(pachd_address.clone()).await?;
+    let mut admin_client = AdminClient::connect(pachd_address.clone()).await?;
     
     for op in ops {
         match op {
             Op::PutFile { flush } => {
-                //
+                let head_commit = pfs::Commit {
+                    repo: Some(pfs::Repo {
+                        name: "fuzz_extract_restore_input".to_string(),
+                    }),
+                    id: "master".to_string(),
+                };
+
+                let mut req = pfs::PutFileRequest::default();
+                req.file = Some(pfs::File {
+                    path: "/test".to_string(),
+                    commit: Some(head_commit.clone()),
+                });
+                req.value = format!("{}", (*COUNTER).fetch_add(1, Ordering::Relaxed)).into_bytes();
+                req.overwrite_index = Some(pfs::OverwriteIndex {
+                    index: 0
+                });
+
+                let req_stream = stream::iter(vec![req]);
+                pfs_client.put_file(req_stream).await?;
+
+                if flush {
+                    pfs_client.flush_commit(pfs::FlushCommitRequest {
+                        commits: vec![head_commit],
+                        to_repos: vec![],
+                    }).await?;
+                }
             },
             Op::ExtractRestore { no_objects, no_repos, no_pipelines } => {
-                //
+                let extracted: Vec<admin::Op> = admin_client.extract(admin::ExtractRequest {
+                    url: "".to_string(),
+                    no_objects,
+                    no_repos,
+                    no_pipelines,
+                }).await?.into_inner().try_collect::<Vec<admin::Op>>().await?;
+
+                let reqs: Vec<admin::RestoreRequest> = extracted.into_iter().map(|op| {
+                    admin::RestoreRequest{
+                        op: Some(op),
+                        url: "".to_string()
+                    }
+                }).collect();
+
+                admin_client.restore(stream::iter(reqs)).await?;
             }
         }
     }
