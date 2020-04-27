131d425eb5d311184f19e9cc64f048b58c204ee8
Yusuf Simonson <simonson@gmail.com>
2020-04-27 11:54:38 -0400

    Shell for extract/restore fuzz test

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ SHELL := /bin/bash
 
 PACHYDERM_ROOT?=${GOPATH}/src/github.com/pachyderm/pachyderm
 RUST_PACHYDERM_ROOT?=${PWD}
+PACHD_ADDRESS?="grpc://$(shell minikube ip):30650"
 
-.PHONY: clean init fuzz
+.PHONY: clean init fuzz-extract-restore
 
 clean:
 	rm -rf proto
@@ -14,5 +15,8 @@ proto:
 	./etc/fix_protos.sh
 	cargo build
 
-fuzz:
-	RUST_BACKTRACE=1 PACHD_ADDRESS="grpc://$(shell minikube service proxy-public --url | head -n 1):30650" cargo fuzz run extract_restore
+fuzz-extract-restore:
+	pachctl create repo fuzz_extract_restore_input
+	pachctl create pipeline -f < fuzz/etc/extract_restore_output.json
+	RUST_BACKTRACE=1 PACHD_ADDRESS=$(PACHD_ADDRESS) cargo fuzz run extract_restore
+	yes | pachctl delete all
--- fuzz/fuzz_targets/extract_restore.rs
+++ fuzz/fuzz_targets/extract_restore.rs
@@ -1,43 +1,51 @@
 #![no_main]
 
-extern crate tokio;
+#[macro_use]
+extern crate lazy_static;
 
 use std::env;
 use std::error::Error;
+use std::sync::atomic::AtomicUsize;
+
+use pachyderm::pfs;
+use pachyderm::pfs::api_client::ApiClient as PfsClient;
+use pachyderm::pps;
+// use pachyderm::pps::api_client::ApiClient as PpsClient;
+use pachyderm::admin::api_client::ApiClient as AdminClient;
 
 use libfuzzer_sys::fuzz_target;
 use arbitrary::{Arbitrary, Unstructured};
-use pachyderm::pfs::{api_client::ApiClient as PfsClient, CreateRepoRequest, Repo};
-use futures::executor::block_on;
+use tokio::runtime::Runtime;
+
+lazy_static! {
+    static ref COUNTER: AtomicUsize = AtomicUsize::new(0);
+}
 
 #[derive(Arbitrary, Clone, Debug, PartialEq)]
 pub enum Op {
-    Foo,
-    Bar
+    PutFile { flush: bool },
+    ExtractRestore { no_objects: bool, no_repos: bool, no_pipelines: bool },
 }
 
 async fn run(ops: Vec<Op>) -> Result<(), Box<dyn Error>> {
     let pachd_address = env::var("PACHD_ADDRESS")?;
-
-    let mut client = PfsClient::connect(pachd_address.clone()).await?;
-
-    let request = tonic::Request::new(CreateRepoRequest {
-        // repo: Some(Repo {
-        //     name: "hello-world".into(),
-        // }),
-        repo: None,
-        description: "".into(),
-        update: false,
-    });
-
-    let response = client.create_repo(request).await?;
-
-    println!("Response: {:?}", response);
+    let pfs_client = PfsClient::connect(pachd_address.clone()).await?;
+    let admin_client = AdminClient::connect(pachd_address.clone()).await?;
+    
+    for op in ops {
+        match op {
+            Op::PutFile { flush } => {
+                //
+            },
+            Op::ExtractRestore { no_objects, no_repos, no_pipelines } => {
+                //
+            }
+        }
+    }
 
     Ok(())
 }
 
-// #[tokio::main]
 fuzz_target!(|ops: Vec<Op>| {
-    block_on(run(ops)).unwrap();
+    Runtime::new().unwrap().block_on(run(ops)).unwrap();
 });
