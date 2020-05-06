7668e5b73253efce017803b9f8f93842b94607d5
Yusuf Simonson <simonson@gmail.com>
2020-05-06 12:08:27 -0400

    Several new extract/restore fuzzer features: * Try pipeline updates (potentially with stats and/or reprocessing) * Check job results * Try failed pipelines

--- fuzz/fuzz_targets/extract_restore.rs
+++ fuzz/fuzz_targets/extract_restore.rs
@@ -18,6 +18,15 @@ use tonic::transport::Channel;
 use tonic::{Code, Status};
 use pretty_assertions::assert_eq;
 
+const BASE_PIPELINE_CODE: &'static str = "
+cp /pfs/fuzz_extract_restore_input/* /pfs/out/
+test \"$(cat /pfs/out/test)\" != \"corrupted\"
+";
+
+const UPDATED_PIPELINE_CODE: &'static str = "
+cp /pfs/fuzz_extract_restore_input/* /pfs/out/
+";
+
 fn create_pfs_input(glob: &str, repo: &str) -> pps::Input {
     let mut pfs_input = pps::PfsInput::default();
     pfs_input.glob = glob.into();
@@ -35,6 +44,9 @@ async fn create_pipeline(
     transform_cmd: Vec<&str>,
     transform_stdin: Option<&str>,
     input: pps::Input,
+    update: bool,
+    stats: bool,
+    reprocess: bool
 ) -> Result<(), Status> {
     let mut request = pps::CreatePipelineRequest::default();
     request.pipeline = Some(pps::Pipeline { name: name.into() });
@@ -46,6 +58,9 @@ async fn create_pipeline(
     }
     request.transform = Some(transform);
     request.input = Some(input);
+    request.update = update;
+    request.enable_stats = stats;
+    request.reprocess = reprocess;
 
     pps_client.create_pipeline(request).await?;
     Ok(())
@@ -76,6 +91,23 @@ async fn inspect_commit(pfs_client: &mut PfsClient<Channel>, commit: pfs::Commit
     Ok(commit)
 }
 
+async fn list_jobs(pps_client: &mut PpsClient<Channel>) -> Result<pps::JobInfos, Code> {
+    // just extract out the error code (if any), so that errors can be
+    // compared with `assert_eq`
+    let result = pps_client.list_job(
+        pps::ListJobRequest {
+            pipeline: None,
+            input_commit: Vec::default(),
+            output_commit: None,
+            history: -1,
+            full: true
+        })
+        .await
+        .map_err(|status| status.code())?;
+    let jobs = result.into_inner();
+    Ok(jobs)
+}
+
 async fn extract(admin_client: &mut AdminClient<Channel>, no_objects: bool, no_repos: bool, no_pipelines: bool) -> Result<Vec<admin::Op>, Status> {
     admin_client.extract(admin::ExtractRequest {
         url: "".to_string(),
@@ -94,7 +126,8 @@ struct Options {
 
 #[derive(Arbitrary, Clone, Debug, PartialEq)]
 enum Op {
-    PutFile { flush: bool }
+    PutFile { corrupted: bool, flush: bool },
+    UpdatePipeline { stats: bool, reprocess: bool }
 }
 
 #[derive(Arbitrary, Clone, Debug, PartialEq)]
@@ -106,6 +139,7 @@ enum Outro {
 async fn run(opts: Options) {
     let pachd_address = env::var("PACHD_ADDRESS").expect("No `PACHD_ADDRESS` set");
     let mut counter = 0;
+    let mut last_corrupted = false;
     let mut pfs_client = PfsClient::connect(pachd_address.clone()).await.unwrap();
     let mut pps_client = PpsClient::connect(pachd_address.clone()).await.unwrap();
     let mut admin_client = AdminClient::connect(pachd_address.clone()).await.unwrap();
@@ -124,8 +158,11 @@ async fn run(opts: Options) {
         &mut pps_client,
         "fuzz_extract_restore_output",
         vec!["bash"],
-        Some("cp /pfs/fuzz_extract_restore_input/* /pfs/out/"),
-        create_pfs_input("/*", "fuzz_extract_restore_input")
+        Some(BASE_PIPELINE_CODE),
+        create_pfs_input("/*", "fuzz_extract_restore_input"),
+        false,
+        false,
+        false,
     ).await.unwrap();
 
     let input_head_commit = pfs::Commit {
@@ -148,16 +185,24 @@ async fn run(opts: Options) {
 
     for op in opts.ops.into_iter() {
         match op {
-            Op::PutFile { flush } => {
-                let file_contents = counter;
-                counter += 1;
+            Op::PutFile { corrupted, flush } => {
+                let contents = if corrupted {
+                    last_corrupted = true;
+                    "corrupted".to_string()
+                } else {
+                    let value = counter;
+                    counter += 1;
+                    last_corrupted = false;
+                    format!("{}", value)
+                };
 
                 let mut req = pfs::PutFileRequest::default();
                 req.file = Some(pfs::File {
                     path: "/test".to_string(),
                     commit: Some(input_head_commit.clone()),
                 });
-                req.value = format!("{}", file_contents).into_bytes();
+
+                req.value = contents.into_bytes();
                 req.overwrite_index = Some(pfs::OverwriteIndex {
                     index: 0
                 });
@@ -171,8 +216,21 @@ async fn run(opts: Options) {
                         to_repos: vec![],
                     }).await.unwrap();
                 }
+            },
+            Op::UpdatePipeline { stats, reprocess } => {
+                create_pipeline(
+                    &mut pps_client,
+                    "fuzz_extract_restore_output",
+                    vec!["bash"],
+                    Some(UPDATED_PIPELINE_CODE),
+                    create_pfs_input("/*", "fuzz_extract_restore_input"),
+                    true,
+                    stats,
+                    reprocess
+                ).await.unwrap();
             }
         }
+
     }
 
     if opts.deferred {
@@ -196,6 +254,7 @@ async fn run(opts: Options) {
         Outro::ExtractRestore { no_objects } => {
             let input_commit_before = inspect_commit(&mut pfs_client, input_head_commit.clone()).await;
             let output_commit_before = inspect_commit(&mut pfs_client, output_head_commit.clone()).await;
+            let jobs_before = list_jobs(&mut pps_client).await;
 
             let extracted = extract(&mut admin_client, no_objects, false, false).await.unwrap();
 
@@ -222,8 +281,10 @@ async fn run(opts: Options) {
             // ensure commits remain the same
             let input_commit_after = inspect_commit(&mut pfs_client, input_head_commit.clone()).await;
             let output_commit_after = inspect_commit(&mut pfs_client, output_head_commit.clone()).await;
+            let jobs_after = list_jobs(&mut pps_client).await;
             assert_eq!(input_commit_before, input_commit_after);
             assert_eq!(output_commit_before, output_commit_after);
+            assert_eq!(jobs_before, jobs_after);
 
             // ensure the file contents are restored
             let file_bytes: Vec<Vec<u8>> = pfs_client.get_file(pfs::GetFileRequest {
@@ -235,7 +296,14 @@ async fn run(opts: Options) {
                 size_bytes: 0,
             }).await.unwrap().into_inner().try_collect::<Vec<Vec<u8>>>().await.unwrap();
             let file_bytes = file_bytes.into_iter().flatten().collect::<Vec<u8>>();
-            assert_eq!(file_bytes, format!("{}", counter).into_bytes());
+
+            if last_corrupted {
+                assert_eq!(file_bytes, "corrupted".to_string().into_bytes());
+            } else {
+                assert_eq!(file_bytes, format!("{}", counter - 1).into_bytes());
+            }
+
+            // TODO: check that jobs status is what we expect
         }
     }
 
