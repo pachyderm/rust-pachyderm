0c16e5cf6d575b1b43f2f85461bd48d928fca7b9
Yusuf Simonson <simonson@gmail.com>
2020-05-06 13:52:37 -0400

    Commit-related operations

--- fuzz/fuzz_targets/pfs.rs
+++ fuzz/fuzz_targets/pfs.rs
@@ -44,6 +44,7 @@ impl Options {
 
         let mut repo_count = 0;
         let mut branch_count = 0;
+        let mut open_commit_count = 0;
 
         for op in &self.ops {
             match op {
@@ -63,6 +64,48 @@ impl Options {
                         return false;
                     }
                     repo_count -= 1;
+                },
+
+                Op::StartCommit => {
+                    if branch_count == 0 {
+                        return false;
+                    }
+                    branch_count -= 1;
+                    open_commit_count += 1;
+                },
+                Op::FinishCommit => {
+                    if open_commit_count == 0 {
+                        return false;
+                    }
+                    open_commit_count -= 1;
+                },
+                Op::InspectCommit { block_state: _, open } => {
+                    if (*open && open_commit_count == 0) || (!open && branch_count == 0) {
+                        return false;
+                    }
+                },
+                Op::ListCommit { number: _, reverse: _ } => {
+                    if repo_count == 0 {
+                        return false;
+                    }
+                },
+                Op::DeleteCommit { open } => {
+                    if *open {
+                        if open_commit_count == 0 {
+                            return false;
+                        }
+                        open_commit_count -= 1;
+                    } else {
+                        if branch_count == 0 {
+                            return false;
+                        }
+                        branch_count -= 1;
+                    }
+                },
+                Op::FlushCommit => {
+                    if branch_count == 0 {
+                        return false;
+                    }
                 },
 
                 Op::CreateBranch { name, new } => {
@@ -90,6 +133,7 @@ impl Options {
                 Op::DeleteAll => {
                     repo_count = 0;
                     branch_count = 0;
+                    open_commit_count = 0;
                 }
                 _ => {}
             }
@@ -106,14 +150,19 @@ enum Op {
     ListRepo,
     DeleteRepo { force: bool, all: bool },
 
+    StartCommit,
+    FinishCommit,
+    InspectCommit { block_state: BlockState, open: bool },
+    ListCommit { number: u64, reverse: bool },
+    DeleteCommit { open: bool },
+    FlushCommit,
+
     CreateBranch { name: Name, new: bool },
     InspectBranch,
     ListBranch { reverse: bool },
     DeleteBranch { force: bool },
 
     DeleteAll,
-
-    // TODO: commit, file-related RPCs
 }
 
 impl Op {
@@ -157,6 +206,13 @@ impl Arbitrary for Name {
     }
 }
 
+#[derive(Arbitrary, Clone, Debug, PartialEq)]
+pub enum BlockState {
+    Started,
+    Ready,
+    Finished
+}
+
 async fn run(opts: Options) {
     let pachd_address = env::var("PACHD_ADDRESS").expect("No `PACHD_ADDRESS` set");
     let mut pfs_client = PfsClient::connect(pachd_address.clone()).await.unwrap();
@@ -164,6 +220,7 @@ async fn run(opts: Options) {
 
     let mut repos = Vec::new();
     let mut branches = Vec::<(String, String)>::new();
+    let mut open_commits = Vec::<(String, String)>::new();
 
     delete_all(&mut pps_client, &mut pfs_client).await.unwrap();
 
@@ -195,6 +252,90 @@ async fn run(opts: Options) {
                     repo: Some(pfs::Repo { name: repos.pop().unwrap() }),
                     force,
                     all
+                }).await);
+            },
+
+            Op::StartCommit => {
+                let (last_repo, last_branch) = branches.pop().unwrap();
+
+                check(pfs_client.start_commit(pfs::StartCommitRequest {
+                    parent: Some(pfs::Commit {
+                        repo: Some(pfs::Repo { name: last_repo.clone() }),
+                        id: last_branch.clone()
+                    }),
+                    description: "".to_string(),
+                    branch: "".to_string(),
+                    provenance: Vec::default(),
+                }).await);
+
+                open_commits.push((last_repo, last_branch));
+            },
+            Op::FinishCommit => {
+                let (last_repo, last_branch) = open_commits.pop().unwrap();
+
+                let mut req = pfs::FinishCommitRequest::default();
+                req.commit = Some(pfs::Commit {
+                    repo: Some(pfs::Repo { name: last_repo.clone() }),
+                    id: last_branch.clone()
+                });
+
+                check(pfs_client.finish_commit(req).await);
+
+                branches.push((last_repo, last_branch));
+            },
+            Op::InspectCommit { block_state, open } => {
+                let (last_repo, last_branch) = if open {
+                    open_commits.last().unwrap().clone()
+                } else {
+                    branches.last().unwrap().clone()
+                };
+
+                check(pfs_client.inspect_commit(pfs::InspectCommitRequest {
+                    commit: Some(pfs::Commit {
+                        repo: Some(pfs::Repo { name: last_repo }),
+                        id: last_branch,
+                    }),
+                    block_state: match block_state {
+                        BlockState::Started => pfs::CommitState::Started.into(),
+                        BlockState::Ready => pfs::CommitState::Ready.into(),
+                        BlockState::Finished => pfs::CommitState::Finished.into(),
+                    }
+                }).await);
+            },
+            Op::ListCommit { number, reverse } => {
+                let last_repo = repos.last().unwrap().clone();
+
+                check(pfs_client.list_commit(pfs::ListCommitRequest {
+                    repo: Some(pfs::Repo { name: last_repo }),
+                    from: None,
+                    to: None,
+                    number,
+                    reverse,
+                }).await);
+            },
+            Op::DeleteCommit { open } => {
+                let (last_repo, last_branch) = if open {
+                    open_commits.pop().unwrap()
+                } else {
+                    branches.pop().unwrap()
+                };
+
+                check(pfs_client.delete_commit(pfs::DeleteCommitRequest {
+                    commit: Some(pfs::Commit {
+                        repo: Some(pfs::Repo { name: last_repo }),
+                        id: last_branch,
+                    })
+                }).await);
+            },
+            Op::FlushCommit => {
+                let (last_repo, last_branch) = branches.last().unwrap().clone();
+
+                check(pfs_client.flush_commit(pfs::FlushCommitRequest {
+                    commits: vec![pfs::Commit {
+                        repo: Some(pfs::Repo { name: last_repo }),
+                        id: last_branch,
+                    }],
+                    to_repos: Vec::default()
                 }).await);
             },
 
@@ -266,5 +407,6 @@ fuzz_target!(|opts: Options| {
     if !opts.valid() {
         return;
     }
+    println!("{:?}", opts);
     Runtime::new().unwrap().block_on(run(opts));
 });
